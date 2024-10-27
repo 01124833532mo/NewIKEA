a05e55b1ff3ee773b2743e528b4324700f60496c
mohamed_hamdi_mahmoud <01124833532mo>
2024-10-27 18:46:11 +0300

    Send Email

--- LinkDev.IKEA.PL/Helpers/EmailSettings.cs
+++ LinkDev.IKEA.PL/Helpers/EmailSettings.cs
@@ -1,4 +1,6 @@
 ﻿using LinkDev.IKEA.DAL.Entites;
+using System.Net.Mail;
+using System.Net;
 
 namespace LinkDev.IKEA.PL.Helpers
 {
@@ -6,8 +8,13 @@ namespace LinkDev.IKEA.PL.Helpers
 	{
 		public static void SendEmail(Email email)
 		{
-
-		}
+           
+                var client = new SmtpClient("smtp.gmail.com", 587);
+                //client.EnableSsl = false;
+                client.Credentials = new NetworkCredential("mh2339@fayoum.edu.eg", "01122953734");
+                client.Send("mh2339@fayoum.edu.eg", email.Reciepints, email.Subject, email.Body);
+            
+        }
 
 	}
 }
